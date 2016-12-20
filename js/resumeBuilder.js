1a740cbcf7d8fa65c94011f3d7365aa03d9a3eed
Iain Stewart <iain.stewart@sabio.co.uk>
2016-12-20 16:18:00 +0000

    Added comments outlining the four objects to be created

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,49 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+
+// BIO:
+    // name : string
+    // role : string
+    // contacts : an object with
+    //       mobile: string
+    //       email: string 
+    //       github: string
+    //       twitter: string (optional)
+    //       location: string
+    // welcomeMessage: string 
+    // skills: array of strings
+    // biopic: url
+    // display: function taking no parameters
+
+// EDUCATION:
+//     schools: array of objects with
+//          name: string
+//          location: string
+//          degree: string
+//          majors: array of strings
+//          dates: string (works with a hyphen between them)
+//          url: string
+//     onlineCourses: array of objects with
+//          title: string
+//          school: string
+//          date: string (works with a hyphen between them)
+//          url: string
+//     display: function taking no parameters
+
+// WORK:
+//     jobs: array of objects with
+//          employer: string 
+//          title: string 
+//          location: string 
+//          dates: string (Can be 'in progress')
+//          description: string 
+//     display: function taking no parameters
+
+// PROJECTS:
+//     projects: array of objects with
+//           title: string 
+//           dates: string (works with a hyphen between them)
+//           description: string
+//           images: array with string urls
+//     display: function taking no parameters
